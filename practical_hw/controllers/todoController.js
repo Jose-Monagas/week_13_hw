cd825d4fefd2f1d18bd5381de12a317c7756029b
Jose <josemonagas212@gmail.com>
2023-06-19 16:48:58 -0400

    create the createTodo and getTodo controllers

--- practical_hw/controllers/todoController.js
+++ practical_hw/controllers/todoController.js
@@ -12,3 +12,30 @@ exports.getTodos = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+exports.createTodo = async (req, res) => {
+  try {
+    if (!req.body.title) {
+      throw new Error("Title is a mandatory input");
+    }
+
+    const todo = new Todo(req.body);
+    await todo.save();
+    res.json(todo);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findOne({ _id: req.params.id });
+    if (!todo) {
+      throw new Error("Did not find an item to update");
+    } else {
+      res.json(todo);
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
